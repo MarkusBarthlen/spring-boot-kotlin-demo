197d5b8fac7b39adb5c8c2ceb72c8d2f5d0451cb
chenkan <ckbsc@126.com>
2016-04-24 22:09:47 +0800

    /misc/ Kotlin语法学习

--- src/test/kotlin/hello/StudyTests.kt
+++ src/test/kotlin/hello/StudyTests.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
 
     // String Interpolation
     val myName = "ChenKan";
-    println("Name: $myName")
+    println("Name: $myName");
 
     // Read-only list & Filter
     val numbers = listOf(1, 2, 3, 4, 6);
@@ -17,21 +17,44 @@ fun main(args: Array<String>) {
     // Read-only map & Traverse
     val dict = mapOf("a" to "adam", "b" to "ben", "c" to  "chen");
     for ((k, v) in dict) {
-        println("$k -> $v")
+        println("$k -> $v");
     }
 
     // singleton
     println(Resource.size);
 
     // If not null shorthand
-    var files = File("Test").listFiles()
-    println(files?.size)
+    var files = File("Test").listFiles();
+    println(files?.size);
 
-    files = File("/").listFiles()
-    println(files?.size)
+    files = File("/").listFiles();
+    println(files?.size);
+
+    // enum
+    println(State.INIT);
+    println(State.START.desc);
+    println(State.valueOf("END").code);
+    println(getStateByCode(2));
 }
 
 // Creating a singleton
 object Resource {
     val size = "1024KB";
+}
+
+// enum
+enum class State(val code: Int, val desc: String) {
+    INIT(0, "Init"),
+    START(1, "Start"),
+    END(2, "End");
+
+}
+
+fun getStateByCode(code: Int): State {
+    return when (code) {
+        0 -> State.INIT;
+        1 -> State.START;
+        2 -> State.END;
+        else -> throw IllegalArgumentException("Invalid Code");
+    }
 }
